5579608831a9ff2b543eb33bd3fa0077a04958de
최유민 <choiyumin@choeyumin-ui-MacBookPro.local>
2023-05-01 11:30:08 +0900

    feat: Add items on lists (Trailing comma test)

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
 # Test pyhon env
-print'hello'
+
+def print_hello():
+    animals = ['dog', 'cat', 'hamster']
+    foods = ['spagetti', 'pizza', 'bibimbob']
+    names = ['John', 'Jane', 'Gil-dong', 'Dong-eun']
+
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+if __name__ == '__main__':
+    print_hello()
